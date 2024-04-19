fbd4a60a4ad5149e33d09072d9e42e0642c74579
maru <maru@myyahoo.com>
2024-04-19 12:48:14 -0400

    Add mutex to deriveArgon2IDKey

--- api/account/common.go
+++ api/account/common.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"regexp"
+	"sync"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -21,8 +22,14 @@ const (
 	TokenSize = 32
 )
 
-var isValidUsername = regexp.MustCompile(`^\w{1,16}$`).MatchString
+var (
+	isValidUsername = regexp.MustCompile(`^\w{1,16}$`).MatchString
+	argonMtx sync.Mutex
+)
 
 func deriveArgon2IDKey(password, salt []byte) []byte {
+	argonMtx.Lock()
+	defer argonMtx.Unlock()
+
 	return argon2.IDKey(password, salt, ArgonTime, ArgonMemory, ArgonThreads, ArgonKeySize)
 }
