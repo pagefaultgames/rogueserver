54d4d9a389685d2247c2c845210c3e910968d4aa
maru <maru@myyahoo.com>
2024-04-25 16:27:24 -0400

    Set max open MariaDB connections

--- db/db.go
+++ db/db.go
@@ -10,12 +10,14 @@ import (
 var handle *sql.DB
 
 func Init(username, password, protocol, address, database string) error {
-	db, err := sql.Open("mysql", username+":"+password+"@"+protocol+"("+address+")/"+database)
+	var err error
+
+	handle, err = sql.Open("mysql", username+":"+password+"@"+protocol+"("+address+")/"+database)
 	if err != nil {
 		return fmt.Errorf("failed to open database connection: %s", err)
 	}
 
-	handle = db
+	handle.SetMaxOpenConns(1000)
 
 	return nil
 }
