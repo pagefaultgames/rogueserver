9cfb923708b3a47669b0095af748670327a1ac57
Up <upcraftedlp@gmail.com>
2024-05-08 11:27:51 +0200

    add default values for CLI args

--- rogueserver.go
+++ rogueserver.go
@@ -34,10 +34,10 @@ func main() {
 	debug := flag.Bool("debug", false, "use debug mode")
 
 	proto := flag.String("proto", "tcp", "protocol for api to use (tcp, unix)")
-	addr := flag.String("addr", "0.0.0.0", "network address for api to listen on")
+	addr := flag.String("addr", "0.0.0.0:3001", "network address for api to listen on")
 
 	dbuser := flag.String("dbuser", "pokerogue", "database username")
-	dbpass := flag.String("dbpass", "", "database password")
+	dbpass := flag.String("dbpass", "pokerogue", "database password")
 	dbproto := flag.String("dbproto", "tcp", "protocol for database connection")
 	dbaddr := flag.String("dbaddr", "localhost", "database address")
 	dbname := flag.String("dbname", "pokeroguedb", "database name")
