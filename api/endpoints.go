f609ec5ee8663fef717d69dfa3efb19cce26039d
Frederico Santos <frederico.f.santos@tecnico.ulisboa.pt>
2024-08-30 01:16:02 +0100

    feat: Add logging for Discord ID addition in admin Discord link endpoint

--- api/endpoints.go
+++ api/endpoints.go
@@ -695,6 +695,6 @@ func handleAdminDiscordLink(w http.ResponseWriter, r *http.Request) {
 		httpError(w, r, err, http.StatusInternalServerError)
 		return
 	}
-
+	fmt.Printf("%s added discord id %s to username %s", userDiscordId, r.Form.Get("discordId"), r.Form.Get("username"))
 	w.WriteHeader(http.StatusOK)
 }
