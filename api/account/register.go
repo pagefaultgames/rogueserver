e1e075ef1f4970c5bbf6339091e085e919e91e69
Pancakes <patapancakes@pagefault.games>
2025-04-25 19:50:49 -0400

    Update copyright year

--- api/account/register.go
+++ api/account/register.go
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2024  Pagefault Games
+	Copyright (C) 2024 - 2025  Pagefault Games
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
