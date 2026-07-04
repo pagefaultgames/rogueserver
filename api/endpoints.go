85ab8019531377414c1109329583e6f1bc15b23c
Dean <69436131+emdeann@users.noreply.github.com>
2026-07-04 13:53:49 -0700

    Track and compare migrator lists in system data

--- api/endpoints.go
+++ api/endpoints.go
@@ -505,6 +505,11 @@ func handleSystem(w http.ResponseWriter, r *http.Request) {
 				httpError(w, r, fmt.Errorf("session out of date: existing version is greater"), http.StatusBadRequest)
 				return
 			}
+
+			if !savedata.ValidMigrators(system.AppliedMigrators, oldSystem.AppliedMigrators) {
+				httpError(w, r, fmt.Errorf("session out of date: migrators desynced"), http.StatusBadRequest)
+				return
+			}
 		}
 
 		err = savedata.UpdateSystem(db.Store, uuid, system)
